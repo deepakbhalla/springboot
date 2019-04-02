1504b3da9a7227e8c2263cfa63726bcd32e62074
Deepak Bhalla <deepak.dbhalla@gmail.com>
2019-04-02 17:00:59 +0100

    Update SpringbootApplication.java

--- src/main/java/com/example/springbootjpacrudexample/SpringbootApplication.java
+++ src/main/java/com/example/springbootjpacrudexample/SpringbootApplication.java
@@ -2,7 +2,6 @@ package com.example.springbootjpacrudexample;
 
 import java.util.Optional;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,24 +17,5 @@ public class SpringbootApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... arg0) throws Exception {
-		
-		System.out.println("\n----------------------------------------------------------------");
-		System.out.println("Sring boot examples by Deepak");
-		System.out.println("  .   ____          _            __ _ _\n" + 
-	         			" /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\\n" + 
-				        "( ( )\\___ | '_ | '_| | '_ \\/ _` | \\ \\ \\ \\\n" + 
-				        " \\\\/  ___)| |_)| | | | | || (_| |  ) ) ) )\n" + 
-				        "  '  |____| .__|_| |_|_| |_\\__, | / / / /\n" + 
-				        " =========|_|==============|___/=/_/_/_/");
-		System.out.println("\nSpring Boot JPA CRUD Examples");
-		System.out.println("----------------------------------------------------------------\n");
-
-		//findAll();
-		//findByPrimaryKey();
-	}
-
 	
 }
