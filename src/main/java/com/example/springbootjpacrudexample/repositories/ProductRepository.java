e3423f2d4502c2ab1666f5241e18310434a5accd
Deepak Bhalla <deepak.dbhalla@gmail.com>
2019-04-02 16:57:25 +0100

    Update ProductRepository.java

--- src/main/java/com/example/springbootjpacrudexample/repositories/ProductRepository.java
+++ src/main/java/com/example/springbootjpacrudexample/repositories/ProductRepository.java
@@ -1,10 +1,5 @@
 package com.example.springbootjpacrudexample.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.springbootjpacrudexample.entities.Product;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+public interface ProductRepository {
 
 }
