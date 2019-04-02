e5371734ada700fd9788892292f4cb8a986e623d
Deepak Bhalla <deepak.dbhalla@gmail.com>
2019-04-02 16:52:11 +0100

    Update Product.java

--- src/main/java/com/example/springbootjpacrudexample/entities/Product.java
+++ src/main/java/com/example/springbootjpacrudexample/entities/Product.java
@@ -10,23 +10,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "product")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "price")
 	private BigDecimal price;
 	
-	@Column(name = "quantity")
 	private int quantity;
 
 	public int getId() {
