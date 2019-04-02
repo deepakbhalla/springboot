067d2c468ff8df6c664839d05296cc78f10ce380
Deepak Bhalla <deepak.dbhalla@gmail.com>
2019-04-02 17:31:30 +0100

    Update SpringbootApplication.java

--- src/main/java/com/example/springbootjpacrudexample/SpringbootApplication.java
+++ src/main/java/com/example/springbootjpacrudexample/SpringbootApplication.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
 
 	@GetMapping("/api/test")
